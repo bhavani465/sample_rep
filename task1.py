fbe35d216177b1ee20bea65e5982c1d8e8386058
bhavani <abhavani465@gmail.com>
2018-07-16 12:53:07 +0530

    ..txt to json conversion added to task1..

--- task1.py
+++ task1.py
@@ -1,4 +1,5 @@
 import subprocess
+import json
 from os.path import isdir,join
 from os import chdir
 
@@ -12,6 +13,31 @@ def search_file(filename):
     if isdir(filename):
             return True
     return False
+def txt_json(txtfile):
+    # converting textfile to git_log.json file
+    l1 = []
+    l2 = []
+    d = {}
+    jp = open("git_log.json", "w")
+    f = open("C:/Users/barrambelly/PycharmProjects/assignment/Python_adb/"+txtfile, "r")
+    for line in f.readlines():
+        if "commit" in line and len(d) != 0:
+            l2.append(d)
+            d = {}
+        if "commit" in line:
+            l1 = line.split()
+            d[l1[0]] = l1[1]
+        elif "Date" in line:
+            l1 = line.split('Date:')
+            d["Date"] = l1[1].lstrip()[:-1]
+        elif "Author" in line:
+            l1 = line.split('Author:')
+            d["Author"] = l1[1].lstrip()[:-1]
+        elif not line == '\n':
+            d["msg"] = line.lstrip()[:-1]
+    json.dump(l2, jp, indent=4, separators=(',', ':'))
+    jp.close()
+
 
 if not search_file("Python_adb"):
     exe_cmd("git clone https://github.com/mpigelati/Python_adb")
@@ -21,5 +47,4 @@ else:
     exe_cmd("git pull origin master --allow-unrelated-histories")
 
 exe_cmd("git log>g_log.txt")
-#chdir("C:/Users/barrambelly/PycharmProjects/assignment/Python_adb")
-exe_cmd("git push https://github.com/bhavani465/sample_rep.git master")
+txt_json("g_log.txt")
