7c6691ea0544b929599ed3fe6adea768b64024e0
bhavani <abhavani465@gmail.com>
2018-07-16 12:48:10 +0530

    task1 prgm...

--- task1.py
+++ task1.py
@@ -1,5 +1,5 @@
 import subprocess
-from os.path import exists,join
+from os.path import isdir,join
 from os import chdir
 
 def exe_cmd(cmd):
@@ -8,16 +8,12 @@ def exe_cmd(cmd):
     p.wait()
     print(str(output.decode('ascii')))
 
-def search_file(filename, search_path):
-    """Given a search path, find file
-    """
-    paths = search_path.split('/')
-    for path in paths:
-        if exists(join(path, filename)):
+def search_file(filename):
+    if isdir(filename):
             return True
     return False
 
-if not search_file("Python_adb","C:/Users/barrambelly/PycharmProjects/assignment"):
+if not search_file("Python_adb"):
     exe_cmd("git clone https://github.com/mpigelati/Python_adb")
     chdir("C:/Users/barrambelly/PycharmProjects/assignment/Python_adb")
 else:
@@ -25,5 +21,5 @@ else:
     exe_cmd("git pull origin master --allow-unrelated-histories")
 
 exe_cmd("git log>g_log.txt")
-chdir("C:/Users/barrambelly/PycharmProjects/assignment/Python_adb")
+#chdir("C:/Users/barrambelly/PycharmProjects/assignment/Python_adb")
 exe_cmd("git push https://github.com/bhavani465/sample_rep.git master")
